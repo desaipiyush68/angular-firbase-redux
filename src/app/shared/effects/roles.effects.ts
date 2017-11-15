c4dab1464a52d86f4ba0bb7e8dc4a842b887e35f
desaipiyush68 <desaipiyush402@gmail.com>
2017-11-15 12:41:06 +0000

    Update roles.effects.ts

--- src/app/shared/effects/roles.effects.ts
+++ src/app/shared/effects/roles.effects.ts
@@ -1,48 +1,59 @@
-import { Injectable }                 from '@angular/core';
-import { Effect, Actions }            from '@ngrx/effects';
-import { AngularFireDatabase }        from 'angularfire2/database';
-import { Observable }                 from 'rxjs/Observable';
-import { of }                         from 'rxjs/observable/of';
+import { Injectable } from '@angular/core';
+import { Effect, Actions } from '@ngrx/effects';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import * as roleActions from '../actions/roles.actions';
+import { RoleService } from '../services/model/roles.service';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/delay';
-import * as roleActions from '../actions/roles.actions';
+
 export type Action = roleActions.All;
 
 
 @Injectable()
 
 export class RoleEffects {
-  constructor(private actions: Actions, private db: AngularFireDatabase) {}
+  constructor(private actions: Actions,
+    private db: AngularFireDatabase,
+    private roleservice: RoleService) { }
+
+
 
   @Effect()
   getRoleList: Observable<Action> = this.actions.ofType(roleActions.GET_ROLE_LIST)
-    .map((action: roleActions.GetRoleList) => action.payload )
-    .mergeMap(payload => this.db.list(payload))
+    .map((action: roleActions.GetRoleList) => action.payload)
+    .switchMap(payload => this.roleservice.getRolesList())
     .map(role => {
       return new roleActions.GetRoleListSuccess(role);
     });
 
+
+
   @Effect()
   updateRole: Observable<Action> = this.actions.ofType(roleActions.UPDATE_ROLE)
-    .map((action: roleActions.UpdateRole) => action.payload )
-    .mergeMap(payload => of(this.db.list('roles/')
-                         .update(payload.$key , payload)))
+    .map((action: roleActions.UpdateRole) => action.payload)
+    .switchMap(payload => this.roleservice.updateRole(payload.$key, payload))
     .map(() => new roleActions.UpdateRoleSuccess());
+
+
 
   @Effect()
   createRole: Observable<Action> = this.actions.ofType(roleActions.CREAT_ROLE)
-    .map((action: roleActions.CreateRole) => action.payload )
-    .mergeMap(payload => of(this.db.list('roles/')
-                         .push(payload)))
+    .map((action: roleActions.CreateRole) => action.payload)
+    .switchMap(payload => this.roleservice.createRole(payload))
     .map(() => new roleActions.CreateRoleSuccess());
-    
+
+
   @Effect()
   deleteRole: Observable<Action> = this.actions.ofType(roleActions.DELETE_ROLE)
-    .map((action: roleActions.CreateRole) => action.payload )
-    .mergeMap(payload => of(this.db.list('roles/')
-                         .remove(payload.$key)))
-    .map(() => new roleActions.DeleteRoleSuccess()); 
+    .map((action: roleActions.CreateRole) => action.payload)
+    .switchMap(payload => this.roleservice.deleteRole(payload.$key))
+    .map(() => new roleActions.DeleteRoleSuccess());
+
 
 }

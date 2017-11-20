05edbefb378d450b5cee3d631e9bec0d29163a6d
desaipiyush68 <desaipiyush402@gmail.com>
2017-11-20 23:38:20 +0000

    Update users.effects.ts

--- src/app/shared/effects/users.effects.ts
+++ src/app/shared/effects/users.effects.ts
@@ -7,26 +7,25 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/delay';
+
 import * as userActions from '../actions/users.actions';
 export type Action = userActions.All;
-import { UserService } from '../services/model/users.service';
+
 
 
 @Injectable()
-
 export class UserEffects {
   constructor(private actions: Actions, 
-              private db: AngularFireDatabase,
-              private userService:UserService) {}
+              private db: AngularFireDatabase) {}
   
   @Effect() 
   getUserList: Observable<Action> = this.actions.ofType(userActions.GET_USER_LIST)
     .map((action: userActions.GetUserList) => action.payload )
     .mergeMap(payload => this.db.list('/users'))
-    .map(user => {
-     return new userActions.GetUserSuccess(user);
+    .map(payload => {
+     return new userActions.GetUserSuccess(payload);
     });
-  
+   
   @Effect()
   updateUser: Observable<Action> = this.actions.ofType(userActions.UPDATE_USER)
     .map((action: userActions.UpdateUser) => action.payload )
@@ -34,18 +33,20 @@ export class UserEffects {
                          .update(payload.$key,payload)))
     .map(() => new userActions.UpdateUserSuccess());
 
+
   @Effect()
   createUser: Observable<Action> = this.actions.ofType(userActions.CREAT_USER)
     .map((action: userActions.CreateUser) => action.payload )
-    .mergeMap(payload => of(this.db.list('/users')
-                         .push(payload)))
+    .mergeMap(payload => of(this.db.list('/users').push(payload)))
     .map(() => new userActions.CreateUserSuccess());
+
     
   @Effect()
   deleteUser: Observable<Action> = this.actions.ofType(userActions.DELETE_USER)
     .map((action: userActions.DeleteUser) => action.payload )
     .mergeMap(payload => of(this.db.list('/users')
                          .remove(payload.$key)))
-    .map(() => new userActions.DeleteUserSuccess()); 
+    .map(() => new userActions.DeleteUserSuccess());
 
+ 
 }

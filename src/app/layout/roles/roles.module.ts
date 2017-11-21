797ab84cf4326bd27141df4864515fb00cac45e4
desaipiyush68 <desaipiyush402@gmail.com>
2017-11-21 16:33:48 +0000

    Update roles.module.ts

--- src/app/layout/roles/roles.module.ts
+++ src/app/layout/roles/roles.module.ts
@@ -4,13 +4,16 @@ import { RolesPageRoutingModule } from './roles-routing.module';
 import { RolesPageComponent } from './roles.component';
 import { PageHeaderModule } from './../../shared';
 import {ReactiveFormsModule,FormsModule} from "@angular/forms";
+import {NgxPaginationModule} from 'ngx-pagination'; 
+
 @NgModule({
   imports: [
     CommonModule,
     RolesPageRoutingModule,
     PageHeaderModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ],
   declarations: [RolesPageComponent]
 })

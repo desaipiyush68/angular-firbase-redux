100529b0015d7931d76f91e3fb179394fe0edb67
desaipiyush68 <desaipiyush402@gmail.com>
2017-12-06 17:42:31 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyAOxA5AVFhjcr3Mw9y7ImH9P3VutzQOYOA",
-    authDomain: "piyush-eqs.firebaseapp.com",
-    databaseURL: "https://piyush-eqs.firebaseio.com",
-    projectId: "piyush-eqs",
-    storageBucket: "piyush-eqs.appspot.com",
-    messagingSenderId: "533321736928"
-  }
+  production: false
 };
